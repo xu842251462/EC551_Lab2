19e72a3d2f18801eefae3832e8bad3c056f477e7
Bin <39841029+xu842251462@users.noreply.github.com>
2022-11-09 00:38:09 -0500

    Add files via upload

---
+++ ec551_lab2.srcs/sources_1/new/top_module.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2022 09:08:09 PM
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+
+    );
+    
+    Cpu cpu();
+    
+endmodule
