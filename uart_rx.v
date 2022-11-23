9b132e886da1d73942d089ffef22c4e81ea81aa4
Yiminada <113543748+Yiminada@users.noreply.github.com>
2022-11-23 13:56:40 -0500

    Update uart_rx.v

--- uart_rx.v
+++ uart_rx.v
@@ -23,14 +23,14 @@
 module uart_rx(
     input wire       clk,          // Input clock from FPGA, 100 MHz (10 ns)
 	input wire       uart_data_in, // UART Serial Data line in
-	output reg       byte_rxed,    // Signal for when 1 byte has been recevied (high for 1 cycle)
-	output reg [7:0] rx_byte       // Byte of data received
+	output reg [2:0] bit_idx,    // Signal for when 1 byte has been recevied (high for 1 cycle)
+	output wire [7:0] rx_byte       // Byte of data received
    );
 	
 	// UART Standard Rates: 1200, 2400, 4800, 19200, 38400, 57600, 115200
-	parameter UART_RATE = 115200;
+	parameter UART_RATE = 9600;
 	parameter CLK_RATE  = 100000000;  // 100 MHz
-	
+	parameter  CLKS_PER_BIT  = 10417; 
 	// State definitions
 	// Use localparams so these cannot be changed
 	localparam STATE_IDLE         = 3'b000;
@@ -44,30 +44,26 @@ module uart_rx(
 	// Clock Counter
 	// Max value of clk_cnt if Baud rate is changed is 100 MHz / 1200 Baud = 83333
 	// which requires 17 bits to store
-	reg [16:0] clk_cnt = 17'd0;
+	reg [14:0] clk_cnt = 0;
 	
-	reg [2:0] bit_idx = 3'd0; // Bit index to count for bytes
+	//reg [2:0] bit_idx = 0; // Bit index to count for bytes
 	
 	// To address issues of metastability, implement
 	// double register for incoming data
-	reg rx_data_reg = 1'b1;
-	reg rx_data = 1'b1;
-	
-	always @ (posedge clk) begin
-		rx_data_reg <= uart_data_in;
-		rx_data <= rx_data_reg;
-	end
+	reg [7:0]     r_RX_Byte     = 0;
+    reg           r_RX_DV       = 0;
+
 	
 	// RX State Machine
 	always @ (posedge clk) begin
 		case (state)
 			STATE_IDLE: begin
-				byte_rxed <= 1'b0;
-				clk_cnt <= 17'd0;
-				bit_idx <= 3'd0;
+				r_RX_DV <= 0;
+				clk_cnt <= 0;
+				bit_idx <= 0;
 				
 				// Start bit Detection
-				if (rx_data == 1'b0) begin
+				if (uart_data_in == 1'b0) begin
 					state <= STATE_RX_START_BIT;
 				end
 				else begin
@@ -77,9 +73,9 @@ module uart_rx(
 			
 			STATE_RX_START_BIT: begin
 				// Check start bit in middle of bit clock width
-				if (clk_cnt == (((CLK_RATE / UART_RATE) - 1) / 2)) begin
+				if (clk_cnt == (CLKS_PER_BIT - 1) / 2) begin
 					// If Start bit still 0, proceed to rx data
-					if (rx_data == 1'b0) begin
+					if (uart_data_in == 1'b0) begin
 						clk_cnt <= 0; //reset counter
 						state <= STATE_RX_DATA_BITS;
 					end
@@ -90,30 +86,30 @@ module uart_rx(
 				end
 				// Wait for middle of bit width
 				else begin
-					clk_cnt <= clk_cnt + 1'b1; //increment counter
+					clk_cnt <= clk_cnt + 1; //increment counter
 					state <= STATE_RX_START_BIT;  // stay in state
 				end
 			end
 			
 			STATE_RX_DATA_BITS: begin
 				// Wait for bit width
-				if (clk_cnt < ((CLK_RATE / UART_RATE) - 1)) begin
+				if (clk_cnt < (CLKS_PER_BIT - 1)) begin
 					clk_cnt <= clk_cnt + 1'b1; //increment counter
 					state <= STATE_RX_DATA_BITS;  // stay in state
 				end
 				// Sample Data bit
 				else begin
 					clk_cnt <= 0; //reset counter
-					rx_byte[bit_idx] <= rx_data;  // sample serial data
+					r_RX_Byte[bit_idx] <= uart_data_in;  // sample serial data
 					
 					// check if we received 8 bits
 					if (bit_idx < 7) begin
-						bit_idx <= bit_idx + 1'b1;  // increment bit index
+						bit_idx <= bit_idx + 1;  // increment bit index
 						state <= STATE_RX_DATA_BITS; // stay in this state
 					end
 					// received a byte
 					else begin
-						bit_idx <= 3'd0;  // reset bit index
+						bit_idx <= 0;  // reset bit index
 						state <= STATE_RX_STOP_BIT; // move to stop bit state
 					end
 				end
@@ -121,22 +117,25 @@ module uart_rx(
 			
 			STATE_RX_STOP_BIT: begin
 				// Wait for bit width
-				if (clk_cnt < ((CLK_RATE / UART_RATE) - 1)) begin
-					clk_cnt <= clk_cnt + 1'b1; //increment counter
+				r_RX_DV <= 1'b0;
+				if (clk_cnt < (CLKS_PER_BIT - 1)) begin
+				    r_RX_DV <= 1'b1;
+					clk_cnt <= clk_cnt + 1; //increment counter
 					state <= STATE_RX_STOP_BIT;  // stay in state
 				end
 				// Stop bit
 				else begin
-					byte_rxed <= 1'b1; // set byte received flag
+				    
 					clk_cnt <= 0; //reset counter
-					state <= STATE_CLEANUP;  // move on to cleanup state
+					state <= STATE_IDLE;  // move on to cleanup state
 				end
 			end
 			
-			STATE_CLEANUP: begin
+			/*STATE_CLEANUP: begin
 				state <= STATE_IDLE;
-				byte_rxed <= 1'b0;
-			end
+				r_RX_DV <= 1'b0;
+				
+			end*/
 			
 			default: begin
 				state <= STATE_IDLE;
@@ -144,4 +143,6 @@ module uart_rx(
 			
 		endcase	
 	end
+	assign byte_rxed = r_RX_DV;
+	assign rx_byte  = r_RX_Byte;
 endmodule
