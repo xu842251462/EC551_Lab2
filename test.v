81fdf56823e2ae008e2f8a36d17a8a2b74713f51
Bin <39841029+xu842251462@users.noreply.github.com>
2022-11-24 23:59:54 -0500

    Update test.v

--- test.v
+++ test.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/24/2022 05:04:53 PM
-// Design Name: 
-// Module Name: binTObcd_cpu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module binTObcd_cpu(
 
     input      [111:0] bin,
@@ -45,14 +23,22 @@ module binTObcd_cpu(
         
     always @(bin) begin
         bcd=0;		 	
-        for (i=0;i<110;i=i+1) begin					//Iterate once for each bit in input number
-            if (bcd[15:0]  >= 17) bcd[15:0]  = bcd[15:0]  + 3;		//If any BCD digit is >= 5, add three
-            if (bcd[31:16] >= 17) bcd[31:16] = bcd[31:16] + 3;
-            if (bcd[47:32] >= 17) bcd[47:32] = bcd[47:32] + 3;
-            if (bcd[63:48] >= 17) bcd[63:48] = bcd[63:48] + 3;
-            if (bcd[79:64] >= 17) bcd[79:64] = bcd[79:64] + 3;
-            if (bcd[95:80] >= 17) bcd[95:80] = bcd[95:80] + 3;
-            bcd = {bcd[110:0],bin[109 - i]};				//Shift one bit, and shift in proper bit from input 
+        for (i=0; i<112; i=i+1) begin					//Iterate once for each bit in input number
+            if (bin[i] <= 9 && bin[i] >= 0) begin
+                bcd[i] = bin[i];
+            end else if (bin[i] == "a") begin
+                bcd[i] = 0;
+            end else if (bin[i] == "b") begin
+                bcd[i] = 1;
+            end else if (bin[i] == "c") begin
+                bcd[i] = 2;
+            end else if (bin[i] == "d") begin
+                bcd[i] = 3;
+            end else if (bin[i] == "e") begin
+                bcd[i] = 4;
+            end else if (bin[i] == "f") begin
+                bcd[i] = 5;
+            end 
         end
     end
 endmodule
